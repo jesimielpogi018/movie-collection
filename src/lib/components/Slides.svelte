957e48b813a07ca6931995d195f5ede0fb645bd8
Jesimiel Almedejar <jesimielalmedejar1018@gmail.com>
2023-06-24 19:12:12 +0800

    made the page ssr turn off and render completely in client only

---
+++ src/lib/components/Slides.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import { Splide, SplideSlide } from "@splidejs/svelte-splide";
+  export const ssr = false;
+</script>
+
+<p>from component</p>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
   import "../global.css";
+  const date = new Date();
 </script>
 
 <slot />
+<footer class="text-white bg-slate-700">
+  <p class="text-center text-sm">
+    All right reserved {date.getFullYear()}. Built with SvelteKit and
+    TailwindCSS
+  </p>
+</footer>
