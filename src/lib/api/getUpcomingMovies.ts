9eadc17aec2927ab1f525363e085d4ac31a10a9a
Jesimiel Almedejar <jesimielalmedejar1018@gmail.com>
2023-06-25 08:40:43 +0800

    function for calling upcoming movies

---
+++ src/lib/api/getUpcomingMovies.ts
@@ -0,0 +1,42 @@
+import { API_URL } from "./url";
+import type { MovieType } from "./type";
+
+interface GetUpcomingMoviesOptions {
+  page?: number;
+  limit?: number | 20;
+}
+
+export let data = [];
+let prevUrl = "";
+let nextUrl = "";
+
+function prev() {}
+
+function next() {}
+
+export async function getUpcomingMovies(config: GetUpcomingMoviesOptions) {
+  const limit = config.limit ? `limit=${config.limit}` : "";
+  const page = config.page ? `page=${config.page}` : "";
+
+  const params = `${page}&${limit}`;
+
+  // if (MapCaching.has(params)) {
+  //   return MapCaching.get(params) as Array<MovieType>;
+  // }
+
+  const url = `${API_URL}/titles/x/upcoming?${params}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "11d66e7985mshcdf89acab1a7d4ep162ddbjsn66d3871ad974",
+      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
