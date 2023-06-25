11b228afda1b5c2394487b31fbedad161ce9acb0
Jesimiel Almedejar <jesimielalmedejar1018@gmail.com>
2023-06-25 08:38:44 +0800

    added type definition for movie result

---
+++ src/lib/api/type.ts
@@ -0,0 +1,50 @@
+export interface MovieType {
+  _id: string;
+  id: string;
+  primaryImage: {
+    id: string;
+    width: number;
+    height: number;
+    url: string;
+    __typename: string;
+    caption: {
+      plainText: string;
+      __typename: string;
+    };
+  };
+  titleType: {
+    displayableProperty: {
+      value: {
+        plainText: string;
+        __typename: string;
+      };
+      __typename: string;
+    };
+    text: string;
+    id: string;
+    isSeries: boolean;
+    isEpisode: boolean;
+    category: Array<string>;
+    canHaveEpisodes: boolean;
+    __typename: string;
+  };
+  titleText: {
+    text: string;
+    __typename: string;
+  };
+  originalTitleText: {
+    text: string;
+    __typename: string;
+  };
+  releaseYear: {
+    year: number;
+    endYear: number | null;
+    __typename: string;
+  };
+  releaseDate: {
+    day: number;
+    month: number;
+    year: number;
+    __typename: string;
+  };
+}
