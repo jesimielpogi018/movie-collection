7b7d63f52443c0a6301f690fa3229e88c93b237c
Jesimiel Almedejar <jesimielalmedejar1018@gmail.com>
2023-06-27 07:40:35 +0800

    display of the movie working

---
+++ src/lib/components/MovieComponent.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { MovieType } from "../api/type";
+
+  export let movie: MovieType;
+</script>
+
+<div class="border border-solid border-slate-500 rounded">
+  <img
+    class="block"
+    src={movie.primaryImage.url}
+    alt={movie.primaryImage.caption.plainText}
+  />
+
+  <p>{movie.titleText.text}</p>
+</div>
