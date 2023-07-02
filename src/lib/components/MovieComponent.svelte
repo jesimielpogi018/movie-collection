0a89cee8e5bb29d405f775e096e1575a22af44f2
Jesimiel Almedejar <jesimielalmedejar1018@gmail.com>
2023-07-02 19:27:39 +0800

    movie component complete

--- src/lib/components/MovieComponent.svelte
+++ src/lib/components/MovieComponent.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
   import type { MovieType } from "../api/type";
 
-  // export let movie: MovieType;
+  export let movie: MovieType;
 </script>
 
-<div class="dsy-card w-full bg-base-100 shadow-xl rounded-md">
-  <figure>
+<div
+  class="dsy-card w-full bg-base-100 shadow-xl rounded-md hover:bg-slate-600"
+>
+  <figure class="w-full h-80">
     <img
-      src="https://dummyimage.com/600x400/3d3d3d/ffffff.png&text=No+Poster+Available"
-      alt="Shoes"
+      class="object-contain"
+      src={movie.primaryImage.url ||
+        "https://dummyimage.com/600x400/3d3d3d/ffffff.png&text=No+Poster+Available"}
+      alt={movie.primaryImage.caption?.plainText || ""}
     />
   </figure>
   <div class="dsy-card-body">
-    <a href="/" class="dsy-card-title dsy-link dsy-link-hover">Shoes!</a>
-    <div class="dsy-badge dsy-badge-secondary">2023</div>
+    <a href="/" class="dsy-card-title dsy-link dsy-link-hover"
+      >{movie.originalTitleText.text}</a
+    >
+    <div class="dsy-badge dsy-badge-secondary">{movie.releaseYear?.year}</div>
   </div>
 </div>
