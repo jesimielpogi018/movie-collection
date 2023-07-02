93227929a08df0cc655d865c90e2c46463d376eb
Jesimiel Almedejar <jesimielalmedejar1018@gmail.com>
2023-07-02 14:37:11 +0800

    added image placeholder

--- src/lib/components/MovieComponent.svelte
+++ src/lib/components/MovieComponent.svelte
@@ -5,11 +5,13 @@
 </script>
 
 <div class="border border-solid border-slate-500 rounded">
-  <!-- <img
+  <img
     class="block"
-    src={movie.primaryImage?.url || ""}
+    src={"https://dummyimage.com/600x400/3d3d3d/ffffff.png&text=No+Poster+Available" ||
+      movie.primaryImage?.url ||
+      ""}
     alt={movie.primaryImage?.caption?.plainText || ""}
-  /> -->
+  />
 
   <p>{movie.titleText.text}</p>
 </div>
