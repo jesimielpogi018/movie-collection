8b1de6f2f5b76445855ecc384f514311e2f03d5a
Jesimiel Almedejar <jesimielalmedejar1018@gmail.com>
2023-07-02 15:57:42 +0800

    basic layout of the page is completed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,16 +3,57 @@
   const date = new Date();
 </script>
 
-<!-- <nav class="bg-slate-600">
-  <ul>
-    <li>Home</li>
-    <li>Popular</li>
-    <li>Movies</li>
-    <li>Upcoming</li>
-  </ul>
-</nav> -->
+<nav class="dsy-navbar bg-base-100 shadow">
+  <div class="dsy-dropdown dsy-navbar-start lg:hidden">
+    <button tabindex="0" class="btn m-1"
+      ><img
+        src="/feather-icon/menu.svg"
+        alt=""
+        width="32"
+        height="32"
+      /></button
+    >
+    <ul
+      class="dsy-dropdown-content z-[1] dsy-menu p-2 shadow bg-base-100 rounded-box w-52"
+    >
+      <li><a href="/">Home</a></li>
+      <li><a href="/popular">Popular</a></li>
+      <li><a href="/movies">Movies</a></li>
+      <li><a href="/upcoming">Upcoming</a></li>
+    </ul>
+  </div>
+  <div class="hidden dsy-navbar-start lg:block">
+    <ul class="w-full dsy-menu-horizontal p-2 gap-3">
+      <li>
+        <a
+          class="text-lg transition duration-150 ease-in-out hover:text-primary"
+          href="/home">Home</a
+        >
+      </li>
+      <li>
+        <a
+          class="text-lg transition duration-150 ease-in-out hover:text-primary"
+          href="/popular">Popular</a
+        >
+      </li>
+      <li>
+        <a
+          class="text-lg transition duration-150 ease-in-out hover:text-primary"
+          href="/movies">Movies</a
+        >
+      </li>
+      <li>
+        <a
+          class="text-lg transition duration-150 ease-in-out hover:text-primary"
+          href="/upcoming">Upcoming</a
+        >
+      </li>
+    </ul>
+  </div>
+  <div class="dsy-navbar-end" />
+</nav>
 <slot />
-<footer class="bg-slate-600">
+<footer class="bg-base-300">
   <ul class="flex flex-row justify-center items-center flex-wrap gap-x-2">
     <li>
       <a class="text-white hover:text-slate-400" href="/terms-of-services"
